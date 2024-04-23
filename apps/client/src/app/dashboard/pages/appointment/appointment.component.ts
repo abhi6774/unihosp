c294ecd45e74b75d8a0a2d9008e14cb934d37e51
abhi6774 <abhishekmourya6774@gmail.com>
2024-04-23 05:39:59 +0000

    adding profile pic to appointment page

--- apps/client/src/app/dashboard/pages/appointment/appointment.component.ts
+++ apps/client/src/app/dashboard/pages/appointment/appointment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../../../services/profile.service';
 import { CommonModule } from '@angular/common';
 import { LoadingComponent } from '../../../loading/loading.component';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'uni-apo',
@@ -18,7 +19,9 @@ import { LoadingComponent } from '../../../loading/loading.component';
 export class AppointmentComponent implements OnInit {
   paitentProfile$ = this.profileService.current;
 
-  constructor(private profileService: ProfileService) {}
+  userService$ = this.userService.currentUser;
+
+  constructor(private profileService: ProfileService, private userService: UserService) {}
 
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit() {}
